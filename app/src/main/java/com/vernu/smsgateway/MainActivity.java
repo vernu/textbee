d1ec48eab6a0dace4d3d856707db97e4fa1d310a
isra el <vernu1997@gmail.com>
2022-03-20 19:54:28 +0300

    setup a basic ui

--- app/src/main/java/com/vernu/smsgateway/MainActivity.java
+++ app/src/main/java/com/vernu/smsgateway/MainActivity.java
@@ -3,12 +3,37 @@ package com.vernu.smsgateway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch gatewaySwich;
+    private EditText gatewayKeyEditText;
+    private Button updateKeyButton, grantSMSPermissionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gatewaySwich = findViewById(R.id.gatewaySwitch);
+        gatewayKeyEditText = findViewById(R.id.gatewayKeyEditText);
+        updateKeyButton = findViewById(R.id.updateKeyButton);
+        grantSMSPermissionBtn = findViewById(R.id.grantSMSPermissionBtn);
+
+
+        grantSMSPermissionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Pls give permissions", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
