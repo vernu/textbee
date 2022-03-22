116bf54ddf00f98f31bb09863f7933f0bf0d6507
isra el <vernu1997@gmail.com>
2022-03-22 21:28:19 +0300

    SharedPref Helper

---
+++ app/src/main/java/com/vernu/sms/helpers/SharedPreferenceHelper.java
@@ -0,0 +1,47 @@
+package com.vernu.sms.helpers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferenceHelper {
+    private final static String PREF_FILE = "PREF";
+
+
+    static void setSharedPreferenceString(Context context, String key, String value){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    static void setSharedPreferenceInt(Context context, String key, int value){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    static void setSharedPreferenceBoolean(Context context, String key, boolean value){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    static String getSharedPreferenceString(Context context, String key, String defValue){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        return settings.getString(key, defValue);
+    }
+
+
+    static int getSharedPreferenceInt(Context context, String key, int defValue){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        return settings.getInt(key, defValue);
+    }
+
+
+    static boolean getSharedPreferenceBoolean(Context context, String key, boolean defValue){
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        return settings.getBoolean(key, defValue);
+    }
+}
