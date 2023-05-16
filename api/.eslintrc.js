3a7f7be7c47eb113705ea196aada7106a41eda04
isra el <vernu1997@gmail.com>
2023-05-16 09:51:46 +0300

    config: update linter config

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    tsconfigRootDir: __dirname
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
