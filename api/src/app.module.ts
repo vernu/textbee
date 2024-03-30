da9e6375bdc5b5f5d0315a5d7e997e2650929bd8
isra el <vernu1997@gmail.com>
2024-03-30 18:47:31 +0300

    feat(api): throttle all endpoints to 30 requests per minute

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,15 +3,28 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { GatewayModule } from './gateway/gateway.module'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core/constants'
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 30,
+      },
+    ]),
     AuthModule,
     UsersModule,
     GatewayModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
