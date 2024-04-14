ed8ea779a20519b00ef908c1d3e821c5de5214ab
isra el <vernu1997@gmail.com>
2024-04-15 01:51:55 +0300

    chore(android): replace query apiKey auth by header

--- android/app/src/main/java/com/vernu/sms/services/GatewayApiService.java
+++ android/app/src/main/java/com/vernu/sms/services/GatewayApiService.java
@@ -1,19 +1,24 @@
 package com.vernu.sms.services;
 
+import com.vernu.sms.dtos.SMSDTO;
+import com.vernu.sms.dtos.SMSForwardResponseDTO;
 import com.vernu.sms.dtos.RegisterDeviceInputDTO;
 import com.vernu.sms.dtos.RegisterDeviceResponseDTO;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface GatewayApiService {
     @POST("gateway/devices")
-    Call<RegisterDeviceResponseDTO> registerDevice(@Query("apiKey") String apiKey, @Body() RegisterDeviceInputDTO body);
+    Call<RegisterDeviceResponseDTO> registerDevice(@Header("x-api-key") String apiKey, @Body() RegisterDeviceInputDTO body);
 
     @PATCH("gateway/devices/{deviceId}")
-    Call<RegisterDeviceResponseDTO> updateDevice(@Path("deviceId") String deviceId, @Query("apiKey") String apiKey, @Body() RegisterDeviceInputDTO body);
+    Call<RegisterDeviceResponseDTO> updateDevice(@Path("deviceId") String deviceId, @Header("x-api-key") String apiKey, @Body() RegisterDeviceInputDTO body);
+
+    @POST("gateway/devices/{deviceId}/receivedSMS")
+    Call<SMSForwardResponseDTO> sendReceivedSMS(@Path("deviceId") String deviceId, @Header("x-api-key") String apiKey, @Body() SMSDTO body);
 }
