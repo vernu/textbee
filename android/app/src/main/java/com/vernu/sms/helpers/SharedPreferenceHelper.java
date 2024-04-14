cd0c40bffaf1dadc10c6fa54a2fa41a386334ac0
isra el <vernu1997@gmail.com>
2024-04-15 02:13:27 +0300

    chore(android): add a function to clear a shared pref value

--- android/app/src/main/java/com/vernu/sms/helpers/SharedPreferenceHelper.java
+++ android/app/src/main/java/com/vernu/sms/helpers/SharedPreferenceHelper.java
@@ -44,4 +44,11 @@ public class SharedPreferenceHelper {
         SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
         return settings.getBoolean(key, defValue);
     }
+
+    public static void clearSharedPreference(Context context, String key) {
+        SharedPreferences settings = context.getSharedPreferences(PREF_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.remove(key);
+        editor.apply();
+    }
 }
