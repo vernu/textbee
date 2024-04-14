d44555eb8ebca44fea625cdd5f03155d49647068
isra el <vernu1997@gmail.com>
2024-04-15 02:04:47 +0300

    chore(api): update sms payload fields

--- api/src/gateway/gateway.dto.ts
+++ api/src/gateway/gateway.dto.ts
@@ -39,16 +39,64 @@ export class SMSData {
   @ApiProperty({
     type: String,
     required: true,
-    description: 'SMS text',
+    description: 'The message to send',
   })
-  smsBody: string
+  message: string
 
   @ApiProperty({
     type: Array,
     required: true,
-    description: 'Array of phone numbers',
+    description: 'List of phone numbers to send the SMS to',
     example: ['+2519xxxxxxxx', '+2517xxxxxxxx'],
   })
+  recipients: string[]
+
+  // TODO: restructure the Payload such that it contains bactchId, smsId, recipients and message in an optimized way
+  // message: string
+  // bactchId: string
+  // list: {
+  //   smsId: string
+  //   recipient: string
+  // }
+
+  // Legacy fields to be removed in the future
+  // @ApiProperty({
+  //   type: String,
+  //   required: true,
+  //   description: '(Legacy) Will be Replace with `message` field in the future',
+  // })
+  smsBody: string
+
+  // @ApiProperty({
+  //   type: Array,
+  //   required: false,
+  //   description:
+  //     '(Legacy) Will be Replace with `recipients` field in the future',
+  //   example: ['+2519xxxxxxxx', '+2517xxxxxxxx'],
+  // })
   receivers: string[]
 }
 export class SendSMSInputDTO extends SMSData {}
+
+export class ReceivedSMSDTO {
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'The message received',
+  })
+  message: string
+
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'The phone number of the sender',
+  })
+  sender: string
+
+  @ApiProperty({
+    type: Date,
+    required: true,
+    description: 'The time the message was received',
+  })
+  receivedAt: Date
+}
