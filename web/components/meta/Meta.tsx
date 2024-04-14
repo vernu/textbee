07645bc755dfe266d07d3e5bdf494d5358d3f35b
isra el <vernu1997@gmail.com>
2024-04-15 01:27:02 +0300

    chore(web): update meta tags

--- web/components/meta/Meta.tsx
+++ web/components/meta/Meta.tsx
@@ -5,9 +5,19 @@ export default function Meta() {
     <Head>
       <title>TextBee - SMS Gateway</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <meta name='description' content='Android SMS Gateway' />
-      <meta name='keywords' content='android, text, sms, gateway, sms-gateway' />
-      <meta name='author' content='Israel Abebe' />
+      <meta
+        name='description'
+        content={`TextBee is an open-source SMS gateway platform built for Android devices.
+        It allows businesses to send SMS messages from dashboard or API, receiving SMS messages and forwarding them to a webhook,
+        streamlining communication and automating SMS workflows. With its robust features, 
+        TextBee is an ideal solution for CRM's, notifications, alerts, two-factor authentication, and various other use cases.
+        `}
+      />
+      <meta
+        name='keywords'
+        content='android, text, sms, gateway, sms-gateway, open-source foss'
+      />
+      <meta name='author' content='Israel Abebe Kokiso' />
       <link rel='icon' href='/favicon.ico' />
     </Head>
   )
