443a210cb109a9a816554411085b05f45814feec
isra el <vernu1997@gmail.com>
2024-10-26 12:03:14 +0300

    init migration to app router

--- web/components/dashboard/APIKeyAndDevices.tsx
+++ web/components/dashboard/APIKeyAndDevices.tsx
@@ -9,18 +9,18 @@ import GenerateApiKey from './GenerateApiKey'
 export default function APIKeyAndDevices() {
   return (
     <Box backdropBlur='2xl' borderWidth='0px' borderRadius='lg'>
-      <Box maxW='xl' mx={'auto'} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
-        <GenerateApiKey />
-      </Box>
       <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 5, lg: 8 }}>
         <Box backdropBlur='2xl' borderWidth='0px' borderRadius='lg'>
           <ErrorBoundary>
-            <ApiKeyList />
+            <Box maxW='xl' mx={'auto'} pt={5} px={{ base: 2, sm: 4, md: 17 }}>
+              <GenerateApiKey />
+              <DeviceList />
+            </Box>
           </ErrorBoundary>
         </Box>
         <Box backdropBlur='2xl' borderWidth='0px' borderRadius='lg'>
           <ErrorBoundary>
-            <DeviceList />
+            <ApiKeyList />
           </ErrorBoundary>
         </Box>
       </SimpleGrid>
