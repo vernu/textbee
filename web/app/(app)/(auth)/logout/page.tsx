f37001afffbd91ec6580b81ca108cf9e85bc13a4
isra el <vernu1997@gmail.com>
2024-12-10 11:41:16 +0300

    fix(web): fix logout page issues

--- web/app/(app)/(auth)/logout/page.tsx
+++ web/app/(app)/(auth)/logout/page.tsx
@@ -1,12 +1,20 @@
 'use client'
 
-import { signOut } from 'next-auth/react'
+import { Routes } from '@/config/routes'
+import { signOut, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function Logout() {
+  const session = useSession()
+  const router = useRouter()
   useEffect(() => {
-    signOut()
-  }, [])
+    if (session.status === 'authenticated') {
+      signOut()
+    } else {
+      router.push(Routes.login)
+    }
+  }, [router, session.status])
 
   return (
     <div className='text-center min-h-screen flex items-center justify-center'>
