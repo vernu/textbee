b72e586d06345f553d05dbfb9cfbe7ab2caf2e3c
isra el <vernu1997@gmail.com>
2025-03-15 17:24:02 +0300

    chore(api): send reminder emails to new inactive and free users

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common'
+import { forwardRef, Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from './schemas/user.schema'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
+import { BillingModule } from 'src/billing/billing.module'
+import { Device, DeviceSchema } from 'src/gateway/schemas/device.schema'
+import { MailModule } from 'src/mail/mail.module'
 
 @Module({
   imports: [
@@ -11,7 +14,13 @@ import { UsersService } from './users.service'
         name: User.name,
         schema: UserSchema,
       },
+      {
+        name: Device.name,
+        schema: DeviceSchema,
+      },
     ]),
+    forwardRef(() => BillingModule),
+    MailModule,
   ],
   controllers: [UsersController],
   providers: [UsersService],
