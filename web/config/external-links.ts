6fb8c729b723876290da45557ef5da770c46b1fc
isra el <vernu1997@gmail.com>
2025-07-14 21:48:55 +0300

    add linkedin and x links to community page

--- web/config/external-links.ts
+++ web/config/external-links.ts
@@ -3,4 +3,6 @@ export const ExternalLinks = {
   github: 'https://github.com/vernu/textbee',
   discord: 'https://discord.gg/d7vyfBpWbQ',
   polar: 'https://donate.textbee.dev',
+  twitter: 'https://x.com/textbeedotdev',
+  linkedin: 'https://www.linkedin.com/company/textbeedotdev',
 }
