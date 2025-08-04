4c164e6bad2b7a7d9cab939b11a65e8e047fccd3
isra el <vernu1997@gmail.com>
2025-08-04 08:44:58 +0300

    test(api): fix failing test

--- api/src/billing/schemas/checkout-session.schema.ts
+++ api/src/billing/schemas/checkout-session.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { Document, Types } from 'mongoose'
-import { User } from 'src/users/schemas/user.schema'
+import { User } from '../../users/schemas/user.schema'
 
 export type CheckoutSessionDocument = CheckoutSession & Document
 
