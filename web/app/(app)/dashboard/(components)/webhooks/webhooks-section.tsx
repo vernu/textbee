4f64880cf8fd25389e4babf7f1ac506999d8b3b0
isra el <vernu1997@gmail.com>
2025-09-21 23:47:53 +0300

    ui(web): improve webhook delivery history page ui

--- web/app/(app)/dashboard/(components)/webhooks/webhooks-section.tsx
+++ web/app/(app)/dashboard/(components)/webhooks/webhooks-section.tsx
@@ -130,7 +130,7 @@ export default function WebhooksSection() {
             className='w-full sm:w-auto'
           >
             <Bell className='mr-2 h-4 w-4' />
-            Webhook Notification
+            Notification Deliveries
           </Button>
         </div>
       </div>
